74faa27d232cb0c7c711f032057040dbd4a5bc7c
AdrianMendoza07 <135410161+andycmcr@users.noreply.github.com>
2024-01-12 17:58:03 -0500

    add::index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+  if (event.key == "Backspace") {
+    console.log("sirvent");
+  }
+  if (
+    (key >= "0" && key <= "9") ||
+    key === "+" ||
+    key === "-" ||
+    key === "*" ||
+    key === "/" ||
+    key === "."
+  ) {
+    if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+      pantalla.textContent = key;
+    } else {
+      pantalla.textContent += key;
+    }
+  } else if (key === "Enter") {
+    try {
+      pantalla.textContent = eval(pantalla.textContent);
+    } catch {
+      pantalla.textContent = "Error!";
+    }
+  } else if (key === "Backspace") {
+    if (
+      pantalla.textContent.length === 1 ||
+      pantalla.textContent === "Error!"
+    ) {
+      pantalla.textContent = "0";
+    } else {
+      pantalla.textContent = pantalla.textContent.slice(0, -1);
+    }
+  } else if (key === "Delete") {
+    pantalla.textContent = "0";
+  }
+});
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const botonApretado = boton.textContent;
+    console.log("sirve");
+    console.log(boton);
+
+    if (boton.id === "c") {
+      pantalla.textContent = "0";
+      return;
+    }
+
+    if (boton.id === "borrar") {
+      if (
+        pantalla.textContent.length === 1 ||
+        pantalla.textContent === "Error!"
+      ) {
+        pantalla.textContent = "0";
+      } else {
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+      }
+      return;
+    }
+
+    if (boton.id === "igual") {
+      try {
+        pantalla.textContent = eval(pantalla.textContent);
+      } catch {
+        pantalla.textContent = "Error!";
+      }
+      return;
+    }
+
+    if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+      pantalla.textContent = botonApretado;
+    } else {
+      pantalla.textContent += botonApretado;
+    }
+  });
+});
